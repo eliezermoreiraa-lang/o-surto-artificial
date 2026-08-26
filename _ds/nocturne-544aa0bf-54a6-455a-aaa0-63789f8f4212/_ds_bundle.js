958ba140fcc574a83199450262ff099821db06ff
eliezermoreiraa-lang <eliezermoreiraa@gmail.com>
2026-08-26 17:01:10 -0300

    Load strict sequential supporter timeline fix

--- _ds/nocturne-544aa0bf-54a6-455a-aaa0-63789f8f4212/_ds_bundle.js
+++ _ds/nocturne-544aa0bf-54a6-455a-aaa0-63789f8f4212/_ds_bundle.js
@@ -18,7 +18,7 @@
       dashboardStarted = true;
       load('/supporter-dashboard-real-v2.js?v=20260826-4');
       load('/supporter-profile-workflow-v4.js?v=20260826-1');
-      load('/supporter-timeline-fix-v2.js?v=20260826-1');
+      load('/supporter-timeline-fix-v3.js?v=20260826-1');
       return;
     }
     setTimeout(startDashboard, 25);
